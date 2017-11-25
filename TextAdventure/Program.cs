ed84d8666adc8a32e10729c7cb80ac99d1343428
Ellen Dekker <Ellen Dekker>
2017-11-25 16:01:00 +0100

    Add project files.

---
+++ TextAdventure/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            //int i = Convert.ToInt32(Console.ReadLine());
+            //if (i == 0)
+            for(;;)
+            {
+                int i = Convert.ToInt32(Console.ReadLine());
+                if (i != 0)
+                {
+                    Console.WriteLine(i * 2);
+                }
+            
+                if (i == 0)
+                {
+                    break;
+                }
+                  
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
