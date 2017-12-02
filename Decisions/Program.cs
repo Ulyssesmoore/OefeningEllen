3e0c4c191bd52246948ff57b57d45bec417663ed
Ellen Dekker <ellen.dekker@outlook.com>
2017-12-02 11:04:25 +0100

    aanpassing Decisions

--- Decisions/Program.cs
+++ Decisions/Program.cs
@@ -10,18 +10,47 @@ namespace Decisions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bob's Big Giveaway");
-            Console.Write("Choose a door: 1, 2 or 3: ");
-            string userValue = Console.ReadLine();
+            /* 
+             Console.WriteLine("Bob's Big Giveaway");
+             Console.Write("Choose a door: 1, 2 or 3: ");
+             string userValue = Console.ReadLine();
 
-            if (userValue == "1")
-            {
-                string message = "You won a new car!";
-                Console.WriteLine(message);
-            }
+             string message = "";
+
+             if (userValue == "1")
+                 message = "You won a new car!";
+             else if (userValue == "2")
+                 message = "You won a new boat!";
+             else if (userValue == "3")
+                 message = "You won a new cat!";
+             else
+             {
+                 message = "Sorry, we didn't understand. ";
+                 //message = message + "You Lose";
+                 message += "You Lose.";
+             }
+                 Console.WriteLine(message);
+                 Console.ReadLine();
+                 */
+
+
+
+                Console.WriteLine("Bob's Big Giveaway");
+                Console.Write("Choose a door: 1, 2 or 3: ");
+                string userValue = Console.ReadLine();
+
+                string message = (userValue == "1") ? "boat" : "strand of lint";
+            // Console.Write("You won a ");
+            //Console.Write(message);
+            //Console.Write(".");
+            //Console.ReadLine();
+
+            //Console.WriteLine("You wona {0}.", message);
+
+            Console.WriteLine("You enterd: {0}, therefore you won a {1}.", userValue, message);
 
             Console.ReadLine();
 
+            }   
         }
     }
-}
